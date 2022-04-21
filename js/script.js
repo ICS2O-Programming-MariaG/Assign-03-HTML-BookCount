153f9aaa9bcb5b930b9d127f47dd75b6c7c70f40
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-21 23:25:42 +0000

    Added parseInt to output values

--- js/script.js
+++ js/script.js
@@ -15,17 +15,17 @@ function valuesSent() {
   if (pageSize < 2) {
     //First secondary if/then scenario: what to calculate if fontSize is 10 (less than 11)
     if (fontSize < 11) {
-      let wordCount = pageCount * 400;
+      let wordCount = parseInt(pageCount * 400);
       document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
     } 
     //Second secondary if/then scenario: what to calculate/display if fontSize is 12 (greater than 11)
     else if (fontSize > 11) {
-      let wordCount = pageCount * 300;
+      let wordCount = parseInt(pageCount * 300);
       document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
     }
     //Third secondary if/then scenario: what to calculate/display if fontSize is 11 (neither of the other scenarios)
     else {
-      let wordCount = pageCount * 350;
+      let wordCount = parseInt(pageCount * 350);
       document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
     }
   }
@@ -33,17 +33,17 @@ function valuesSent() {
   else if (pageSize > 1) {
     //fontSize is 10
     if (fontSize < 11) {
-      let wordCount = pageCount * 600;
+      let wordCount = parseInt(pageCount * 600);
       document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
     } 
     //fontSize is 12 (greater than 11)
     else if (fontSize > 11) {
-      let wordCount = pageCount * 425;
+      let wordCount = parseInt(pageCount * 425);
       document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
     }
     //fontSize is 11 (neither of the other scenarios)
     else {
-      let wordCount = pageCount * 500;
+      let wordCount = parseInt(pageCount * 500);
       document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
     }
   }
