971fe6834882fc47a6d9d92cb6531462ef08b4a7
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-21 14:29:22 +0000

    Allowed users to input a decimal page count (for example, 1/2 page or 3/4 page)

--- js/script.js
+++ js/script.js
@@ -7,23 +7,44 @@
 "use strict"
 
 function valuesSent() {
-  let pageCount = document.getElementById("book-pages").value;
+  let pageCount = parseFloat(document.getElementById("book-pages").value);
   let fontSize = parseInt(document.getElementById("font-size").value);
-  
-  //First if/then scenario: what to calculate if fontSize is 10 (less than 11)
-  if (fontSize < 11) {
-    let wordCount = pageCount * 400;
-    document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
-  } 
-  //Second if/then scenario: what to calculate/display if fontSize is 12 (greater than 11)
-  else if (fontSize > 11) {
-    let wordCount = pageCount * 300;
-    document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
-  } 
+  let pageSize = parseInt(document.getElementById("page-size").value);
 
-  //Third if/then scenario: what to calculate/display if fontSize is 11 (neither of the other scenarios)
-  else {
-    let wordCount = pageCount * 350;
-    document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+  //First big if/then question: page size option 1 means page size 5" x 8"
+  if (pageSize < 2) {
+    //First secondary if/then scenario: what to calculate if fontSize is 10 (less than 11)
+    if (fontSize < 11) {
+      let wordCount = pageCount * 400;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+    } 
+    //Second secondary if/then scenario: what to calculate/display if fontSize is 12 (greater than 11)
+    else if (fontSize > 11) {
+      let wordCount = pageCount * 300;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+    }
+    //Third secondary if/then scenario: what to calculate/display if fontSize is 11 (neither of the other scenarios)
+    else {
+      let wordCount = pageCount * 350;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+    }
+  }
+  //Second big if/then option: page size option 2 means page size 6" x 9"
+  else if (pageSize > 1) {
+    //fontSize is 10
+    if (fontSize < 11) {
+      let wordCount = pageCount * 600;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+    } 
+    //fontSize is 12 (greater than 11)
+    else if (fontSize > 11) {
+      let wordCount = pageCount * 425;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+    }
+    //fontSize is 11 (neither of the other scenarios)
+    else {
+      let wordCount = pageCount * 500;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+    }
   }
 }
