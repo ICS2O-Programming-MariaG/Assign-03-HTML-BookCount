cc1de12bddc49dda68a3150d65316e357e9a91fd
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-23 13:53:16 +0000

    Fixed way of rounding calculations, added extra white space at the bottom of the page

--- js/script.js
+++ js/script.js
@@ -11,40 +11,40 @@ function valuesSent() {
   let fontSize = parseInt(document.getElementById("font-size").value);
   let pageSize = parseInt(document.getElementById("page-size").value);
 
-  //First big if/then question: page size option 1 means page size 5" x 8"
+  //First big if/then question: page size option 1 (represented by < 2) means page size 5" x 8"
   if (pageSize < 2) {
     //First secondary if/then scenario: what to calculate if fontSize is 10 (less than 11)
     if (fontSize < 11) {
-      let wordCount = parseInt(pageCount * 400);
-      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+      let wordCount = pageCount * 400;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount.toFixed(0) + " words.";
     } 
     //Second secondary if/then scenario: what to calculate/display if fontSize is 12 (greater than 11)
     else if (fontSize > 11) {
-      let wordCount = parseInt(pageCount * 300);
-      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+      let wordCount = pageCount * 300;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount.toFixed(0) + " words.";
     }
     //Third secondary if/then scenario: what to calculate/display if fontSize is 11 (neither of the other scenarios)
     else {
-      let wordCount = parseInt(pageCount * 350);
-      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+      let wordCount = pageCount * 350;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount.toFixed(0) + " words.";
     }
   }
   //Second big if/then option: page size option 2 means page size 6" x 9"
   else if (pageSize > 1) {
     //fontSize is 10
     if (fontSize < 11) {
-      let wordCount = parseInt(pageCount * 600);
-      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+      let wordCount = pageCount * 600;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount.toFixed(0) + " words.";
     } 
     //fontSize is 12 (greater than 11)
     else if (fontSize > 11) {
-      let wordCount = parseInt(pageCount * 425);
-      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+      let wordCount = pageCount * 425;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount.toFixed(0) + " words.";
     }
     //fontSize is 11 (neither of the other scenarios)
     else {
-      let wordCount = parseInt(pageCount * 500);
-      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount + " words.";
+      let wordCount = pageCount * 500;
+      document.getElementById("results").innerHTML = "The approximate word count of the book is " + wordCount.toFixed(0) + " words.";
     }
   }
 }
